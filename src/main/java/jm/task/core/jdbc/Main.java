7e87957983d4a4b254e2d97a78277df4058fa925
Dmitry <abashidzedima@mail.ru>
2022-04-27 17:20:47 +0300

    aa

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,30 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connection conn= Util.connect();
+        Statement stat = conn.createStatement();
+        stat.executeUpdate("drop TABLE IF EXISTS User1");
+        stat.executeUpdate("CREATE TABLE IF NOT EXISTS user1 (id INT(64) NOT NULL AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age INT(64), PRIMARY KEY(id));");
+        stat.executeUpdate("insert into user1 (name, lastName, age) VALUES ('Dima','Abashidze',20),('Ivan','Armaturovich',30),('Abob','Abobovich',40),('Biba','Bobovich',50)");
+        ResultSet rs1 = stat.executeQuery("select * from user1");
+        while(rs1.next()) {
+            System.out.println(rs1.getInt(1));
+            System.out.println(rs1.getString(2));
+            System.out.println(rs1.getString(3));
+            System.out.println(rs1.getInt(4));
+            System.out.println("-----------------------");
+        }
+        stat.executeUpdate("DELETE FROM user1 WHERE id = 3");
+        stat.executeUpdate("DELETE FROM user1");
+
+
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
-    // реализуйте настройку соеденения с БД
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util{
+    private static String userName = "root";
+    private static String password = "1234";
+    private static String connectionURL="jdbc:mysql://localhost:3306/mysql?useUnicode=true&serverTimezone=UTC";;// реализуйте алгоритм здесь
+    public static Connection connect() throws SQLException, ClassNotFoundException {
+        Connection connection = DriverManager.getConnection(connectionURL, userName, password);
+            System.out.println("We're connected");
+            return connection;// реализуйте настройку соеденения с БД
+
+    }
 }
