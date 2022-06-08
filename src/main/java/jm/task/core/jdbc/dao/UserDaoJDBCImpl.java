c9f11b5f3072d53a258441d0961671532b0eaa9e
Dmitry <abashidzedima@mail.ru>
2022-06-08 21:36:41 +0300

    aa

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -13,56 +13,66 @@ import java.util.List;
 public class UserDaoJDBCImpl implements UserDao {
 
     Connection conn= Util.connect();
-    Statement stat;
-    List<User> user;
 
     public UserDaoJDBCImpl() throws SQLException, ClassNotFoundException {
-        user = new ArrayList<>();
+
     }
 
     public void createUsersTable() throws SQLException, ClassNotFoundException {
-        stat = conn.createStatement();
-        stat.executeUpdate("CREATE TABLE IF NOT EXISTS user1 (id INT(64) NOT NULL AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age INT(64), PRIMARY KEY(id));");
-        stat.close();
+        try(Statement stat = conn.createStatement()) {
+            stat.executeUpdate("CREATE TABLE IF NOT EXISTS user1 (id INT(64) NOT NULL AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age INT(64), PRIMARY KEY(id));");
+        }catch(Exception e){
+            System.out.println("Error");
+        }
     }
 
     public void dropUsersTable() throws SQLException {
-        stat = conn.createStatement();
-        stat.executeUpdate("drop TABLE IF EXISTS User1");
-        stat.close();
+        try(Statement stat = conn.createStatement()){
+            stat.executeUpdate("drop TABLE IF EXISTS User1");
+        }catch(Exception e){
+            System.out.println("Error");
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-        stat = conn.createStatement();
-        stat.executeUpdate("insert into user1 (name, lastName, age) VALUES (\""+ name + "\",\"" + lastName + "\","+ age + ")");
-        stat.close();
+        try(Statement stat = conn.createStatement()) {
+            stat.executeUpdate("insert into user1 (name, lastName, age) VALUES (\"" + name + "\",\"" + lastName + "\"," + age + ")");
+        }catch(Exception e){
+            System.out.println("Error");
+        }
     }
 
     public void removeUserById(long id) throws SQLException {
-        stat = conn.createStatement();
-        stat.executeUpdate("DELETE FROM user1 WHERE id = id");
-        stat.close();
+        try(Statement stat = conn.createStatement()) {
+            stat.executeUpdate("DELETE FROM user1 WHERE id = id");
+        }catch(Exception e){
+            System.out.println("Error");
+        }
     }
 
     public List<User> getAllUsers() throws SQLException {
-        stat = conn.createStatement();
-        ResultSet rs1 = stat.executeQuery("select * from user1");
-        user = new ArrayList<>();
-        User user2 = new User();
-        while(rs1.next()) {
-            user2.setName(rs1.getString("name"));
-            user2.setLastName(rs1.getString("lastName"));
-            user2.setAge(rs1.getByte("age"));
-            user2.setId(1L);
-            user.add(user2);
+        List<User> user = new ArrayList<>();
+        try(Statement stat = conn.createStatement()){
+            ResultSet rs1 = stat.executeQuery("select * from user1");
+            User user2 = new User();
+            while(rs1.next()) {
+                user2.setName(rs1.getString("name"));
+                user2.setLastName(rs1.getString("lastName"));
+                user2.setAge(rs1.getByte("age"));
+                user2.setId(1L);
+                user.add(user2);
+            }
+        }catch (Exception e){
+            System.out.println("Error");
         }
-        stat.close();
         return user;
     }
 
     public void cleanUsersTable() throws SQLException {
-        stat = conn.createStatement();
-        stat.executeUpdate("DELETE FROM user1");
-        stat.close();
+        try(Statement stat = conn.createStatement()) {
+            stat.executeUpdate("DELETE FROM user1");
+        }catch(Exception e){
+            System.out.println("Error");
+        }
     }
 }
